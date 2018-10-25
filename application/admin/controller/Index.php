97d929114b68b2c373bea20bea9c98185c3ae4fe
gu <846061943@qq.com>
2018-10-25 15:29:01 +0800

    后台模板搭建

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,23 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+/**
+ *后台相关页面
+ */
+class Index extends Controller
+{
+    public function index()
+    {
+       	return $this->fetch();
+    }
+
+    public function login()
+    {
+       	return $this->fetch();
+    }
+
+    public function welcome()
+    {
+       	return $this->fetch();
+    }
+}
